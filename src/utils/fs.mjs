81484e51e5e072d5336a1f4ddffe791afaf30f4b
pavgurin <kalliak1993@yandex.ru>
2021-04-20 19:11:53 +0300

    changed getList logic - for serials returns SERIAL status that will be used to dead with single series

--- src/utils/fs.mjs
+++ src/utils/fs.mjs
@@ -7,7 +7,6 @@ export const readdir = util.promisify(fs.readdir);
 export const readFile = util.promisify(fs.readFile);
 export const unlink = util.promisify(fs.unlink);
 export const rmdir = util.promisify(fs.rmdir);
-export const { statSync } = fs;
 
 export const getPendingFilmsPath = (...args) => path.join(config.pendingFilmsPath, ...args);
 
